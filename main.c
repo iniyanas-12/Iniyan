a46a20e73d1d89a4b46e4e558ccf484726dc523b
https://github.com/iniyanas-12 <iniyanas@yantravision.com>
2025-02-20 13:04:45 +0530

    added sub()

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int add(int a,int b){return a+b;}
+int sub(int a,int b){return b-a;}
 int main(){
-   printf("Sum:%d\n,add(3,4)");
+   printf("Sum:%d \n",add(3,4));
+   printf("sub:%d \n",sub(4,3));
    return 0
 }
