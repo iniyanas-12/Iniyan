2bf7489fac29fad9480ad3e055069bdc54b5805f
athul-yv <athul.krishna@yantravision.com>
2025-02-20 13:24:37 +0530

    included div()

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return b - a; }
 int mul(int a, int b) { return a * b; }
-int div(int a, int b) { return a * b; }
+int div(int a, int b) { return a / b; }
 int main() {
    printf("Sum: %d\n", add(3, 4));
    printf("Subtraction: %d\n", sub(4, 3));
