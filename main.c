64c24ace6a3564d8c48616e5720b63765cfc434a
madheshwaran-s <madheshwaran.s@yantravision.com>
2025-02-20 13:11:02 +0530

    added multiplication function

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-int add(int a,int b){return a+b;}
-int sub(int a,int b){return b-a;}
-int main(){
-   printf("Sum:%d \n",add(3,4));
-   printf("sub:%d \n",sub(4,3));
-   return 0
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return b - a; }
+int mul(int a, int b) { return a * b; }
+int main() {
+   printf("Sum: %d\n", add(3, 4));
+   printf("Subtraction: %d\n", sub(4, 3));
+   printf("Multiplication: %d\n", mul(3, 4));
+   return 0;
 }
